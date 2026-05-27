40671a661172260b9e693c09b9f110687b34ab24
mr0xb <47467008+mr0xb@users.noreply.github.com>
2026-05-27 11:39:25 -0400

    updates

---
+++ .zshrc.d/work.zsh
@@ -0,0 +1,9 @@
+source ~/.config/.vault
+
+export AWS_VAULT_BACKEND=keychain
+export AWS_VAULT_KEYCHAIN_NAME="aws-vault"
+export AWS_LOGIN_BROWSER="firefox"
+
+#export KUBECONFIG=/Users/steve.briggs/.kube/config:/Users/steve.briggs/.kube/minikube
+export KUBECONFIG="${HOME}/.kube/teleport-kubeconfig:${HOME}/.kube/config:${HOME}/.kube/minikube"
+export PATH=$PATH:/private/var/folders/ky/m6p32md91_n2q6z94fdn8fsw0000gq/T/AppTranslocation/F252CD76-5A71-4D7F-8826-12175DB43E32/d/Visual\ Studio\ Code.app/Contents/Resources/app/bin
